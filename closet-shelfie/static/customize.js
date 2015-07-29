a10956c7484cea3de040c970815f468f36690a38
Michelle <michelle.y.xie@gmail.com>
2015-07-29 11:11:28 -0700

    updated custom page

--- closet-shelfie/static/customize.js
+++ closet-shelfie/static/customize.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
   $(my_outfit).click(function(){
     $(.my_outfit).fadeout();
   });
+
+  $("#top").mouseover(function(){
+    $(this).animate(
+      {opacity:0.5},
+    10
+    );
 });

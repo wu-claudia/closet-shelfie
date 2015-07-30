46e82664fdfed997bbc984559aa9593c4eca46f1
Michelle <michelle.y.xie@gmail.com>
2015-07-30 12:19:50 -0700

    updated js

--- closet-shelfie/static/customize.js
+++ closet-shelfie/static/customize.js
@@ -7,10 +7,20 @@ $(document).ready(function(){
     );
   });
 
-  $("#color").change(function(e){
-    $.map($('div.access.color'), function(e){$('.'+e.value).hide(); });
-    $('.'+$('div.access.color').val()).show()
-    });
+
+  $(function(){
+  $("#color").bind("change", function() {
+      var value = $(this).find("option:selected").val();
+      // var last3chars = value.substring(value.length - 3);
+  $("div[class]").hide();
+      $(".class_" + value).show();
+  });
+  });
+
+  // $("#color").change(function(e){
+  //   $.map($('div.access.color'), function(e){$('.'+e.value).hide(); });
+  //   $('.'+$('div.access.color').val()).show()
+  //   });
   //   $("div#FilterContainer").find("div").hide();
   //   $("div#FilterContainer").find("div." + filters).show();
   // });

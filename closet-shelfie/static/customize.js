d9f2df2e98bce566f678299a49921dd417f6b606
Michelle <michelle.y.xie@gmail.com>
2015-07-30 09:53:30 -0700

    all css updated

--- closet-shelfie/static/customize.js
+++ closet-shelfie/static/customize.js
@@ -4,9 +4,11 @@ $(document).ready(function(){
     $(.my_outfit).fadeout();
   });
 
-  $("#top").mouseover(function(){
-    $(this).animate(
-      {opacity:0.5},
-    10
-    );
+  $("select.filterby").change(function(){
+    var filters = $.map($("select.filterby").toArray(),function(e){
+      return $(e).val();
+    }).join(".");
+    $("div#FilterContainer").find("div").hide();
+    $("div#FilterContainer").find("div." + filters).show();
+  });
 });

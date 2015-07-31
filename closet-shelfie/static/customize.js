ca3b8e142345770c6a5bb199ca18cdf701b7c604
Michelle <michelle.y.xie@gmail.com>
2015-07-31 09:17:25 -0700

    js fixed

--- closet-shelfie/static/customize.js
+++ closet-shelfie/static/customize.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 
-  $("#top").hover(function(){
-    $(this).animate(
-      {opacity:0.5},
-    10
-    );
-  });
-
 
   $("#color").change(function() {
       var value = $(this).val();
@@ -29,12 +22,4 @@ $(document).ready(function(){
     $.post( "/delete", { item: $(this).val()} );
   });
 
-
-  // $("#color").change(function(e){
-  //   $.map($('div.access.color'), function(e){$('.'+e.value).hide(); });
-  //   $('.'+$('div.access.color').val()).show()
-  //   });
-  //   $("div#FilterContainer").find("div").hide();
-  //   $("div#FilterContainer").find("div." + filters).show();
-  // });
 });
